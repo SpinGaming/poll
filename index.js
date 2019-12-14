a7d7ebf63aea01c3d9b4e5a5eba77616af65af6a
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-14 23:07:30 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Client, RichEmbed } = require('discord.js');
+const bot = new Client();
+
+const token = "NjU1NDczNjM3ODQyODc4NDY0.XfUnbA._6jXRj1L65OB0gXnRfQJjEOwNTs";
+
+const PREFIX = "p!";
+
+bot.on('ready', () => {
+    console.log("The bot is ready!");
+});
+
+bot.on('message', message => {
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0]) {
+
+        case "poll":
+            const Embed = new RichEmbed()
+                .setColor(0xFFC300)
+                .setTitle("Poll Help")
+                .setDescription("p!poll to initiate a simple yes/no poll!")
+
+            if (!args[1]) {
+                message.channel.send(Embed);
+                break;
+            }
+
+            let msgArgs = args.slice(1).join(" ");
+
+            message.channel.send("🗳️" + "**" + msgArgs + "**").then(messageReaction => {
+                messageReaction.react("👍");
+                messageReaction.react("👎");
+                message.delete(2000).catch(console.error);
+            });
+
+            break;
+    }
+
+});
+
+bot.login(token);
